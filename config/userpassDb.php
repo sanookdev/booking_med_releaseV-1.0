4470acb3ce660d495c725dd05d6ea01f32693963
Mr.Nuk <104189137+sanookdev@users.noreply.github.com>
2023-01-11 11:50:55 +0700

    Update userpassDb.php

--- config/userpassDb.php
+++ config/userpassDb.php
@@ -1,12 +1,6 @@
 <?
 $hostDb = "localhost";
-// $hostDb = "192.168.66.17";
 $userDb = "root";
-// $userDb = "medtu";
 $passDb = "";
-// $passDb = "tmt@medtu";
-// $nameDb = "techno_booking";
 $nameDb = "med_booking";
-// $hostDb = "localhost";
-
 ?>
